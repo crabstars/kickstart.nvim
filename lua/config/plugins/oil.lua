de1744ff53c4e04349b45528b6ba906fb4b536b4
kami <danielwaechtler@protonmail.com>
2024-12-20 10:41:32 +0100

    add oil

---
+++ lua/config/plugins/oil.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  },
+}
+-- start with :e .
+-- start with :e /paht/to/location
